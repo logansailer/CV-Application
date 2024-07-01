ad0e67f6a0ab016e3eab629dd6fdd009452c634c
Logan Sailer <logansailer0633@gmail.com>
2024-07-01 15:13:40 +0900

    fixed app(it was broken)

--- CV-Application/src/App.jsx
+++ CV-Application/src/App.jsx
@@ -1,8 +1,15 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [personalInfo, setPersonalInfo] = useState(exampleData.personalInfo);
 
+  return (
+    <div className="app">
+      <div className="editor"></div>
+      <div className="form-container"></div>
+    </div>
+  );
 }
 
-export default App
+export default App;
